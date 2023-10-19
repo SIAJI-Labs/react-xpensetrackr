55a8e05638fc701313006b9c4c978482173eca6f
Dwi Aji <si.aji03@gmail.com>
2023-10-19 13:28:35 +0700

    Modify welcome page

---
+++ resources/js/Layouts/PublicLayout.tsx
@@ -0,0 +1,10 @@
+import { Link } from '@inertiajs/react';
+import { PropsWithChildren } from 'react';
+
+export default function Guest({ children }: PropsWithChildren) {
+    return (
+        <div className="min-h-screen w-full">
+            {children}
+        </div>
+    );
+}

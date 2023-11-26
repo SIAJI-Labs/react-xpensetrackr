b1a97eb581b6aa0f84a1318ca0ee6cfed9239402
Dwi Aji <si.aji03@gmail.com>
2023-11-26 19:15:45 +0700

    Add notification and sidebar panel, etc

---
+++ public/build/assets/InputLabel-d21fb6b4.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-0e733e7a.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};

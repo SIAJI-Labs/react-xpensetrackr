e80d8887f675a4647ad3ae0c039fa5920bd59857
Dwi Aji <si.aji03@gmail.com>
2023-12-08 13:43:12 +0700

    Build the asset

---
+++ public/build/assets/skeleton-5603228f.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-18d9f852.js";import{c as o}from"./button-9dd4167c.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};

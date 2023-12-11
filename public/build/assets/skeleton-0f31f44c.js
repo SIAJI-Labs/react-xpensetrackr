27deedd3c91910668db22027cbbd9dc39e4a20aa
Dwi Aji <si.aji03@gmail.com>
2023-12-11 22:32:58 +0700

    Latest build

---
+++ public/build/assets/skeleton-0f31f44c.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-7bc3d3ae.js";import{c as o}from"./button-fcc7bb2d.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};

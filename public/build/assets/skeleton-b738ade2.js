545dd1c38468077b9295af15bdd9a7b8b302cd22
Dwi Aji <si.aji03@gmail.com>
2023-12-21 10:34:49 +0700

    Recent build

---
+++ public/build/assets/skeleton-b738ade2.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-ca991f1d.js";import{c as o}from"./button-01b89d3a.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
---
+++ public/build/assets/x-fa66cc73.js
@@ -0,0 +1,1 @@
+import{b as e}from"./button-01b89d3a.js";const o=e("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);export{o as X};

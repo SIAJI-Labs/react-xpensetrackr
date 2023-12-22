98058ba780286c41727177a82adc635f06a014a7
Dwi Aji <si.aji03@gmail.com>
2023-12-22 16:58:34 +0700

    Add quote command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        // Generate first log for the day
+        $schedule->call(function () {
+            Log::info('Initialize debug file - '.date('Y-m-d H:i:s').' ~ \App\Console\Kernel@schedule');
+        })->dailyAt('00:00');
+
+        // Fetch Inpsiration of the day
+        $schedule->command('inspire:get-quotes')->dailyAt('00:00');
     }
 
     /**
